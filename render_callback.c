745424fae9ff52f6eafa1e8370e87fbfda1f7618
Serhii Piddubchak <serhii.piddubchak@toatech.com>
2013-03-01 18:34:07 +0200

    generate singed int data instead of float

--- render_callback.c
+++ render_callback.c
@@ -8,20 +8,20 @@ OSStatus myRenderer(void *inRefCon,
                   UInt32 inNumberFrames,
                   AudioBufferList *ioData)
 {
-  double currPhase = *((double *)inRefCon);
-  Float32 *outputBuffer = (Float32 *)ioData->mBuffers[0].mData;
-  const double frequency = 2.;
+	double currPhase = *((double *)inRefCon);
+  int *outputBuffer = (int *)ioData->mBuffers[0].mData;
+  const double frequency = 440.;
   const double phaseStep = (frequency / 44100.) * (M_PI * 2.);
   
   for (int i = 0; i < inNumberFrames; i++)
   {
-    outputBuffer[i] = sin(currPhase);
+    outputBuffer[i] = (int)(sin(currPhase)*32768) << 16 | (int)(sin(currPhase)*32768);
     currPhase += phaseStep;
   }
 
   *((double *)inRefCon) = currPhase;
 
-  printf("ioData address: %p; inNumberFrames: %d; flags: %d\n", ioData, inNumberFrames, *ioActionFlags);
+  printf("currPhase: %f; inNumberFrames: %d; outputBuffer(%lu): %d\n", currPhase, inNumberFrames, sizeof(outputBuffer[0]), *((int *)outputBuffer));
   return noErr;
 }
 
